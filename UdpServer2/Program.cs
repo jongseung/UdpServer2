c619180964c0683f6ebc5899918cadc451824196
jongseung <park930902@gmail.com>
2019-07-18 11:13:58 +0900

    다중채팅완성

--- UdpServer2/Program.cs
+++ UdpServer2/Program.cs
@@ -18,7 +18,24 @@ namespace UdpServer2
     {
         static void Main(string[] args)
         {
-         
+            UdpClient recver = new UdpClient(12000);
+
+            IPEndPoint src_ip = new IPEndPoint(0, 0);
+            byte[] recv_data;
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+            string result;
+            for(; ; )
+            {
+                recv_data = recver.Receive(ref src_ip);
+                stream.Write(recv_data, 0, recv_data.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                result = (string)formatter.Deserialize(stream);
+                Console.WriteLine("수신된 데이터 : {0}", result);
+                stream.SetLength(0);
+            }
+
+            recver.Close();
         }
     }
 }
